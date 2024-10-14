1e8de0a7ce8bd34b5c28140801b96844c85dcbad
suna <py0831@naver.com>
2024-10-14 15:17:46 +0900

    Article Entity 생성 및 초기데이터 세팅 완료

---
+++ src/main/java/com/my/articles/entity/article.java
@@ -0,0 +1,23 @@
+package com.my.articles.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class article {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 50)
+    private String title;
+
+    @Column(nullable = false, length = 1000)
+    private String content;
+}
